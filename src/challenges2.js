9d7157a23e117ac10dbded13e0c9e48a82fefb64
Samuel Constantino <samuelicapuidados@gmail.com>
2021-04-29 09:35:55 -0300

    implementa requisito 11: mascara de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,46 @@ function techList(listaDeTecnologias, name) {
 // console.log(techList(listaDeTecnologias, 'Samuel'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(listaDeNumeros) {
+  if (listaDeNumeros.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  let numeroDeRepeticoes;
+  let valorNegativoOuMaiorQueNove = false;
+  for (let value of listaDeNumeros) {
+    numeroDeRepeticoes = 0;
+    for (let value2 of listaDeNumeros) {
+      if (value == value2) {
+        numeroDeRepeticoes += 1;
+      }
+    }
+    if (value < 0 || value > 9) {
+      valorNegativoOuMaiorQueNove = true;
+    }
+    if (numeroDeRepeticoes >= 3 || valorNegativoOuMaiorQueNove) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let listaFormatada = '('
+  for (let key in listaDeNumeros) {
+    if (key == 0) {
+      listaFormatada += listaDeNumeros[key];
+    } else if (key == 1) {
+      listaFormatada += listaDeNumeros[key] + ') '
+    } else if (key > 1 && key <= 6){
+      listaFormatada += listaDeNumeros[key];
+    } else if (key >= 7) {
+      if (key == 7) {
+        listaFormatada += '-' + listaDeNumeros[key];
+      } else {
+        listaFormatada += listaDeNumeros[key];
+      }
+    }
+  }
+  return listaFormatada;
 }
+let listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(listaDeNumeros));
 
 // Desafio 12
 function triangleCheck() {
